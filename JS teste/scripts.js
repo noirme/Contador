87df6d09724ea9404202a7138da127fea8f33e3a
noirme <101916119+noirme@users.noreply.github.com>
2022-07-18 19:31:20 -0300

    Add files via upload

---
+++ JS teste/scripts.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+var currentNumber = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	currentNumber.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	currentNumber.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
